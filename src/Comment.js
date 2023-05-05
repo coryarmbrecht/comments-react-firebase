6d55c7b7c3fd4e3614df3149c8517cce47b242a1
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-05-05 20:53:51 +0500

    Completed

--- src/Comment.js
+++ src/Comment.js
@@ -1,71 +1,5 @@
-// import React, { Component } from "react";
-// import _ from "lodash";
-import "./style.scss";
-
-// export default class Comment extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       comment: this.props.comment || "Nopes.",
-//       hello: "world",
-//       visualState: "--empty",
-//     };
-//   }
-//   componentWillMount = () => {
-//     /*
-// 		console.log('Comment componentWillMount')
-//         */
-//     setTimeout(() => {
-//       //console.log('Comment componentDidMount')
-//       //
-//     }, 500);
-//   };
-//   componentDidMount = () => {
-//     console.log("Comment componentDidMount");
-
-//     this.setState({ visualState: "" });
-//     setTimeout(() => {
-//       //console.log('Comment componentDidMount')
-
-//       console.log("visualState = ", this.state.visualState);
-//     }, 1500);
-//   };
-//   componentWillUnMount = () => {
-//     //console.log('Comment componentWillUnMount')
-//     setTimeout(() => {
-//       //this.setState({visualState:'--exit'})
-//     }, 2500);
-//   };
-
-//   componentWillReceiveProps = (newProps) => {
-//     //console.log('Comment componentWillReceiveProps newProps.comment = ', newProps.comment)
-//     //console.log('Comment componentWillReceiveProps this.state.comment = ', this.state.comment)
-//     if (newProps.comment !== this.state.comment) {
-//       console.log(
-//         "componentWillReceiveProps newProps is new = ",
-//         newProps.comment
-//       );
-//       setTimeout(() => {
-//         console.log("Comment componentWillReceiveProps --edited");
-//         this.setState({ visualState: "--edited" });
-//       }, 500);
-//     } else {
-//       //console.log('componentWillReceiveProps newProps is not new = ', newProps.comment)
-//     }
-//   };
-
-//   render = () => {
-//     //console.log('Comment render')
-//     return (
-//       <div className={"comment" + this.state.visualState}>
-//         UserImage UserName Time {this.state.visualState}
-//         <p>{this.props.comment}</p>
-//       </div>
-//     );
-//   };
-// }
-
 import React, { useState } from "react";
+import "./style.scss";
 
 const Comment = ({ comment }) => {
   const [animate, setAnimate] = useState("");
@@ -76,6 +10,7 @@ const Comment = ({ comment }) => {
       setAnimate(false);
     }, 2000);
   };
+
   return (
     <div className={`comment ${animate && "tada"}`} onClick={handleClick}>
       <p>{comment}</p>
--- src/Comments.js
+++ src/Comments.js
@@ -1,168 +1,7 @@
-// import React, { Component } from "react";
-// import ReactCSSTransitionGroup from "react-addons-css-transition-group";
-// import _ from "lodash";
-// import moment from "moment";
-// // import timeago from "timeago.js";
-// // import { FbAppDB } from "./firebasesetup";
-// import { FbAppDb } from "./firebasesetup";
-// import { collection } from "firebase/firestore";
-
+import React, { useEffect, useState } from "react";
 import { fadeInUp, shake, tada } from "react-animations";
-// import { StyleSheet, css } from "aphrodite";
 import "./style.scss";
-// // import Comment from "./Comment";
 import Comment from "./Comment";
-// // import Transition from "react-transition-group/Transition";
-// const commentsDB = collection(FbAppDb, "comments");
-
-// // const messagesRef = FBApp.ref("messages/");
-
-// export default class Comments extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       loaded: false,
-//       newComment: "New Comment",
-//       store: {},
-//       comment: "New Comment",
-//       comments: [
-//         { comment: "Change this page...", uid: "159djb" },
-//         { comment: "Add this feature...", uid: "45adb" },
-//         { comment: "You have a bug that...", uid: "z5djb" },
-//       ],
-//       commentsRef: commentsDB,
-//       theme: null,
-//       toggled: false,
-//     };
-//   }
-//   componentWillMount = () => {
-//     console.log("componentWillMount this.state = ", this.state);
-//     //this.setState({comments:this.retrievedComments})
-
-//     let retrievedTopComments = this.state.commentsRef.on(
-//       "value",
-//       (snapshot) => {
-//         //console.log('retirevedComments snapshot = ', snapshot.val());
-
-//         const snapshotComments = _.values(snapshot.val());
-//         //console.log('snapshotComments = ', snapshotComments)
-//         const snapshotCommentsKeys = _.keys(snapshot.val());
-//         //console.log('snapshotCommentsKeys = ', snapshotCommentsKeys)
-//         console.log("loaded:true");
-//         //let snapshotCommentsArray = Object.keys(snapshotComments);
-//         //console.log('snapshotCommentsArray = ', snapshotCommentsArray)
-//         this.setState({ comments: _.reverse(snapshotComments), loaded: true });
-//       },
-//       function (error) {
-//         //console.log("Error: " + error.code);
-//       }
-//     );
-//   };
-//   onBlur = (event) => {
-//     console.log("onBlur event.target.value = ", event.target.value);
-//     this.setState({ newComment: event.target.value });
-//   };
-//   suggest = () => {
-//     console.log("suggested");
-//     console.log("newComment = ", this.state.newComment);
-//     let newCommentKey = this.state.commentsRef.push().key;
-
-//     this.state.commentsRef.child(newCommentKey).update({
-//       comment: this.state.newComment,
-//       ts: moment.now(),
-//       uid: newCommentKey,
-//     });
-//   };
-//   componentWillUnmount = () => {
-//     this.state.commentsRef.off("value", (offval) => {
-//       console.log("componentWillUnmount = ", offval);
-//     });
-//   };
-//   /*
-// 	shouldComponentUpdate = (newprops) => {
-// 		console.log('Comments shouldComponentUpdate, newprops = ', newprops)
-// 		if(this.state.loaded === true){
-
-// 			return false
-// 		}
-// 		return true
-
-//     }
-//     */
-//   // componentWillReceiveProps = (newProps) => {
-//   //   console.log("Comments shouldComponentUpdate, newprops = ", newprops);
-//   // };
-//   render = () => {
-//     console.log("comments render this.state =", this.state);
-//     return (
-//       <div styles={{ height: "100%", backgroundColor: "coral" }}>
-//         <h2
-//           styles={{
-//             color: "white",
-//             fontSize: 20,
-//             textAlign: "center",
-//             margin: 0.2,
-//           }}
-//         >
-//           Comments
-//         </h2>
-//         {/*
-// 				<ReactCSSTransitionGroup
-// 					transitionName="listAnim"
-// 					transitionEnterTimeout={1500}
-// 					transitionLeaveTimeout={500}
-// 					transitionAppear={true}
-// 					transitionAppearTimeout={1000}
-// 				>
-// 				<div styles={{alignSelf:'center', backgroundColor:'white','overflowY':'scroll'}}>
-//                     {
-//                         this.state.comments.map((comment, index) => {
-//                             //console.log('comment = ', comment)
-//                             return <li key={index} ><Comment comment={comment.comment}  /></li>
-//                         })
-//                     }
-// 				</div>
-// 				</ReactCSSTransitionGroup>
-// 				*/}
-//         <div
-//           className={css(styles.list)}
-//           styles={{ alignSelf: "center", overflowY: "scroll" }}
-//         >
-//           <ReactCSSTransitionGroup
-//             transitionName="fadr"
-//             transitionEnterTimeout={3000}
-//             transitionLeaveTimeout={1000}
-//             transitionAppear={true}
-//             transitionAppearTimeout={1000}
-//           >
-//             {this.state.comments.map((comment) => {
-//               console.log("comment = ", comment);
-//               return (
-//                 <li key={comment.uid}>
-//                   <Comment comment={comment.comment} />
-//                 </li>
-//               );
-//             })}
-//           </ReactCSSTransitionGroup>
-//         </div>
-//         <div>
-//           <label htmlFor="name">Name:</label>
-//           <input
-//             id="name"
-//             type="text"
-//             onBlur={this.onBlur}
-//             placeholder="Enter your comment here..."
-//           />
-//           <button onClick={this.suggest} title="Submit">
-//             Submit Comment
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   };
-// }
-
-import React, { useEffect, useRef, useState } from "react";
 import { FbAppDb } from "./firebasesetup";
 import {
   collection,
@@ -189,7 +28,6 @@ const Comments = () => {
       const arr = [];
       querySnapshot.forEach((doc) => {
         arr.push(doc.data());
-        console.log("snapshot ", arr);
       });
       setComments(arr);
     });
@@ -230,10 +68,9 @@ const Comments = () => {
         className={css(styles.list)}
         styles={{ alignSelf: "center", overflowY: "scroll" }}
       >
-        {comments?.map((comment) => {
-          console.log("comment = ", comment);
+        {comments?.map((comment, i) => {
           return (
-            <li key={comment.id} className={`fade-in-up`}>
+            <li key={i} className={`fade-in-up`}>
               <Comment comment={comment.comment} />
             </li>
           );
